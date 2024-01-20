18426b15c9b7c70031697d7c32504ef5a45d7fa7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-19 20:25:44 -0800

    nit: Tweaks to editor config

--- komorebi.sample.ps1
+++ komorebi.sample.ps1
@@ -1,9 +1,9 @@
-if (!(Get-Process whkd -ErrorAction SilentlyContinue))
-{
-    Start-Process whkd -WindowStyle hidden
+
+if (!(Get-Process whkd -ErrorAction SilentlyContinue)) {
+  Start-Process whkd -WindowStyle hidden
 }
 
-. $PSScriptRoot\komorebi.generated.ps1
+. "$PSScriptRoot/komorebi.generated.ps1"
 
 # Send the ALT key whenever changing focus to force focus changes
 komorebic alt-focus-hack enable
@@ -20,23 +20,23 @@ komorebic ensure-named-workspaces 0 I II III IV V
 # komorebic ensure-named-workspaces 1 A B C D E F
 
 # Assign layouts to workspaces, possible values: bsp, columns, rows, vertical-stack, horizontal-stack, ultrawide-vertical-stack
-komorebic named-workspace-layout I bsp
+komorebic named-workspace-layout I vertical-stack
 
 # Set the gaps around the edge of the screen for a workspace
-komorebic named-workspace-padding I 20
+komorebic named-workspace-padding I 5
 # Set the gaps between the containers for a workspace
-komorebic named-workspace-container-padding I 20
+komorebic named-workspace-container-padding I 5
 
 # You can assign specific apps to named workspaces
 # komorebic named-workspace-rule exe "Firefox.exe" III
 
 # Configure the invisible border dimensions
-komorebic invisible-borders 7 0 14 7
+komorebic invisible-borders 4 4 4 4
 
 # Uncomment the next lines if you want a visual border around the active window
-# komorebic active-window-border-colour 66 165 245 --window-kind single
-# komorebic active-window-border-colour 256 165 66 --window-kind stack
-# komorebic active-window-border-colour 255 51 153 --window-kind monocle
-# komorebic active-window-border enable
+komorebic active-window-border-colour 66 165 245 --window-kind single
+komorebic active-window-border-colour 256 165 66 --window-kind stack
+komorebic active-window-border-colour 255 51 153 --window-kind monocle
+komorebic active-window-border enable
 
 komorebic complete-configuration
